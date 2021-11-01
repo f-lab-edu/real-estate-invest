f229298ec28b45ec9f55396d7f856c166b95dd4f
jerry92k <jens92.k@gmail.com>
2021-11-01 14:01:31 +0900

    test(UserController) : 인증테스트

--- src/main/java/kancho/realestate/comparingprices/controller/UserController.java
+++ src/main/java/kancho/realestate/comparingprices/controller/UserController.java
@@ -1,5 +1,6 @@
 package kancho.realestate.comparingprices.controller;
 
+import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import kancho.realestate.comparingprices.domain.dto.request.UserDto;
 import kancho.realestate.comparingprices.domain.model.User;
@@ -19,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final UserService userService;
+	private static final String SESSION_KEY="userNo";
 
 	@GetMapping("/test")
 	public String test(){
@@ -27,7 +30,6 @@ public class UserController {
 
 	@PostMapping("/join")
 	public String join(@RequestBody UserDto requestUserDto){
-		System.out.println("here inside");
 		userService.createUser(requestUserDto);
 		return "회원가입 완료";
 	}
@@ -35,7 +37,15 @@ public class UserController {
 	@PostMapping("/login")
 	public String login(HttpSession session, @RequestBody UserDto requestUserDto){
 		User loginUser = userService.login(requestUserDto);
-		session.setAttribute("userNo",loginUser.getUserNo());
+		session.setAttribute(SESSION_KEY,loginUser.getUserNo());
 		return "로그인 완료";
+	}
+
+	@GetMapping("/authTest")
+	public String authTest(HttpSession session) throws AuthenticationException {
+		if(session.getAttribute(SESSION_KEY)==null){
+			throw new AuthenticationException("로그인이 필요합니다.");
+		}
+		return "인증 테스트";
 	}
 }
