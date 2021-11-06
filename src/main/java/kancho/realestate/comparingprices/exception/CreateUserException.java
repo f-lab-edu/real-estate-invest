e147fd254a74b151b84c20266bbec0b8b51b8db0
jerry92k <jens92.k@gmail.com>
2021-11-06 23:20:58 +0900

    chore: 미사용 예외 클래스 삭제

--- src/main/java/kancho/realestate/comparingprices/exception/CreateUserException.java
+++
@@ -1,8 +0,0 @@
-package kancho.realestate.comparingprices.exception;
-
-public class CreateUserException extends RuntimeException {
-
-	public CreateUserException(String message) {
-		super(message);
-	}
-}
