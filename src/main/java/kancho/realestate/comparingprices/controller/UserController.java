f408bc5c03e51ae2f1d52af5af3865ffc0889186
jerry92k <jens92.k@gmail.com>
2021-11-06 22:57:46 +0900

    test: 회원가입, 로그인 Controller 성공 테스트

--- src/main/java/kancho/realestate/comparingprices/controller/UserController.java
+++ src/main/java/kancho/realestate/comparingprices/controller/UserController.java
@@ -3,12 +3,15 @@ package kancho.realestate.comparingprices.controller;
 import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kancho.realestate.comparingprices.domain.dto.request.UserDto;
+import kancho.realestate.comparingprices.domain.dto.response.SuccessReponse;
 import kancho.realestate.comparingprices.domain.model.User;
 import kancho.realestate.comparingprices.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -20,21 +23,16 @@ public class UserController {
 	private final UserService userService;
 	public static final String SESSION_KEY="userNo";
 
-	@PostMapping("/join")
-	public String join(@RequestBody UserDto requestUserDto){
+	@PostMapping(value = "/join", produces = "application/json; charset=utf8")
+	public ResponseEntity join(@RequestBody UserDto requestUserDto){
 		userService.createUser(requestUserDto);
-		return "회원가입 완료";
+		return new ResponseEntity<>(new SuccessReponse<>("회원가입 완료",""), HttpStatus.CREATED);
 	}
 
-	@PostMapping("/login")
-	public String login(HttpSession session, @RequestBody UserDto requestUserDto){
+	@PostMapping(value ="/login", produces = "application/json; charset=utf8")
+	public ResponseEntity login(HttpSession session, @RequestBody UserDto requestUserDto){
 		User loginUser = userService.login(requestUserDto);
 		session.setAttribute(SESSION_KEY,loginUser.getUserNo());
-		return "로그인 완료";
-	}
-
-	@GetMapping("/test")
-	public String test(){
-		return "test";
+		return new ResponseEntity<>(new SuccessReponse<>("회원가입 완료",""), HttpStatus.CREATED);
 	}
 }
---
+++ src/test/java/kancho/realestate/comparingprices/controller/UserControllerTest.java
@@ -0,0 +1,63 @@
+package kancho.realestate.comparingprices.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	UserController userController;
+
+	@Test
+	@Transactional
+	public void join_회원가입_성공() throws Exception {
+
+		HashMap<String,String> bodyContent = new HashMap<>();
+		bodyContent.put("id","jerry");
+		bodyContent.put("password","1234");
+		String serializedBody = String.valueOf(new JSONObject(bodyContent));
+
+		mockMvc.perform(post("/join")
+			.content(serializedBody)
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated());
+	}
+
+	@Test
+	@Transactional
+	public void login_로그인_성공() throws Exception {
+
+		HashMap<String,String> bodyContent = new HashMap<>();
+		bodyContent.put("id","jerry");
+		bodyContent.put("password","1234");
+		String serializedBody = String.valueOf(new JSONObject(bodyContent));
+
+		mockMvc.perform(post("/join")
+			.content(serializedBody)
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated());
+
+		mockMvc.perform(post("/login")
+			.content(serializedBody)
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated());
+	}
+}
