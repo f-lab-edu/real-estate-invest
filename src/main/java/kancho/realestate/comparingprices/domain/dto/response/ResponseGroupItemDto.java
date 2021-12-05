3b48463cf19ff57869a5ef1b7271c3f3cf4a1259
jerry92k <jens92.k@gmail.com>
2021-12-05 23:20:30 +0900

    feat: ObjectMapper 파싱을 위해 기본생성자 추가

--- src/main/java/kancho/realestate/comparingprices/domain/dto/response/ResponseGroupItemDto.java
+++ src/main/java/kancho/realestate/comparingprices/domain/dto/response/ResponseGroupItemDto.java
@@ -1,6 +1,5 @@
 package kancho.realestate.comparingprices.domain.dto.response;
 
-import kancho.realestate.comparingprices.domain.model.ComparingGroup;
 import kancho.realestate.comparingprices.domain.model.GroupItem;
 import lombok.Getter;
 
@@ -10,6 +9,9 @@ public class ResponseGroupItemDto {
 	private Long id;
 	private Long groupId;
 	private Long apartmentId;
+
+	public ResponseGroupItemDto() {
+	}
 
 	public ResponseGroupItemDto(Long id, Long groupId, Long apartmentId) {
 		this.id = id;
