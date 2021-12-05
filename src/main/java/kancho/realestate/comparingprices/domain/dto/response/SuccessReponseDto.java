5c2d4b45499b32f1aa662a29718ad11515b9055e
jerry92k <jens92.k@gmail.com>
2021-12-05 23:18:36 +0900

    fix: ObjectMapper에서 파싱을 위해 기본 생성자 추가

--- src/main/java/kancho/realestate/comparingprices/domain/dto/response/SuccessReponseDto.java
+++ src/main/java/kancho/realestate/comparingprices/domain/dto/response/SuccessReponseDto.java
@@ -1,11 +1,17 @@
 package kancho.realestate.comparingprices.domain.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class SuccessReponseDto<T> {
     private String message;
     private T result;
+
+    public SuccessReponseDto() {
+    }
+
+    public SuccessReponseDto(String message, T result) {
+        this.message = message;
+        this.result = result;
+    }
 }
