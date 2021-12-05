a2092f852403c18076c5fe3d6736d0914e2840f0
jerry92k <jens92.k@gmail.com>
2021-12-05 21:53:10 +0900

    feat : 메서드명 변경

--- src/test/java/kancho/realestate/comparingprices/repository/UserMapperTest.java
+++ src/test/java/kancho/realestate/comparingprices/repository/UserMapperTest.java
@@ -3,14 +3,10 @@ package kancho.realestate.comparingprices.repository;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import kancho.realestate.comparingprices.domain.model.User;
@@ -27,7 +23,7 @@ class UserMapperTest {
 	@CsvSource(value = {"tom2542:23lg354232"}, delimiter = ':')
 	void 사용자_등록(String id, String password) {
 		User user = new User(id,password);
-		userMapper.insertUser(user);
+		userMapper.saveUser(user);
 		Optional<User> createdUser = userMapper.selectUserById(id);
 		Assertions.assertThat(createdUser.isPresent()).isEqualTo(true);
 		Assertions.assertThat(createdUser.get().getId()).isEqualTo(id);
