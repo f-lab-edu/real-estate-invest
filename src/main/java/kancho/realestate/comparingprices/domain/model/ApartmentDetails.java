017dd8e29b5ceddd8bbed8db7da6ee1a27c5b5a4
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-12-24 15:06:39 +0900

    feat: 아파트와 아파트 가격 정보를 입력할 클래스 생성

---
+++ src/main/java/kancho/realestate/comparingprices/domain/model/ApartmentDetails.java
@@ -0,0 +1,22 @@
+package kancho.realestate.comparingprices.domain.model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@XmlRootElement(name = "items")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApartmentDetails {
+	@XmlElement(name = "item")
+	private List<ApartmentDetail> apartmentDetailList;
+}
