92d020df0d2c247d9c82b9b159ef6b8f5b1708f3
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-12-24 15:11:35 +0900

    feat: 서울시의 구별 지역코드를 관리하는 enum 클래스 추가

---
+++ src/test/java/kancho/realestate/utils/api/storeaprtment/model/GuTest.java
@@ -0,0 +1,27 @@
+package kancho.realestate.utils.api.storeaprtment.model;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import kancho.realestate.comparingprices.domain.model.Gu;
+
+class GuTest {
+
+	@Test
+	void 존재하는_지역번호를_찾으면_true_반환() {
+		String existingRegionalCode = "11680";
+
+		boolean isCorrect = Gu.isCorrectRegionalCode(existingRegionalCode);
+
+		assertThat(isCorrect).isTrue();
+	}
+
+	@Test
+	void 존재하지_않는_지역번호를_찾으면_false_반환() {
+		String existingRegionalCode = "4444";
+
+		boolean isCorrect = Gu.isCorrectRegionalCode(existingRegionalCode);
+
+		assertThat(isCorrect).isFalse();
+	}
+}
