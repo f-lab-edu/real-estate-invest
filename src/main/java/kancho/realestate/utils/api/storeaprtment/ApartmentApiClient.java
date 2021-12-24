2b8aafa01107d9087518b5d360aebf7c427ab22d
Shim Seoung Chul <tlatmdcjf94@gmail.com>
2021-12-24 15:14:46 +0900

    feat: 공공데이터 API에 아파트, 아파트 정보를 요청하는 클래스 - 가져온 xml 형식의 데이터를 리스트 객체 형태로 반환한다.

---
+++ src/main/java/kancho/realestate/utils/api/storeaprtment/ApartmentApiClient.java
@@ -0,0 +1,77 @@
+package kancho.realestate.utils.api.storeaprtment;
+
+import static java.net.URLEncoder.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import kancho.realestate.comparingprices.domain.dto.request.RequestApartmentDetailDto;
+import kancho.realestate.comparingprices.domain.model.ApartmentDetail;
+
+@Component
+public class ApartmentApiClient {
+	@Value("${data-go-kr.apartment-price-detail.accessKey}")
+	private String accessKey;
+	@Value("${data-go-kr.domain-name}")
+	private String domainName;
+	@Value("${data-go-kr.apartment-price-detail.path}")
+	private String path;
+
+	private final String encoder = "UTF-8";
+	private final String pageNo = "1"; // 페이지번호
+	private final String numOfRows = "1000"; // 한 페이지 결과 수
+
+	public List<ApartmentDetail> getApartmentDetails(RequestApartmentDetailDto requestDto) throws IOException, JAXBException {
+			URL url = new URL(makeRequestUrl(requestDto));
+			String responseAsXmlString = request(url); // 공공 API에 데이터 요청
+			return new ApartmentXmlMapper().apartmentXmlToList(responseAsXmlString);
+	}
+
+	private String makeRequestUrl(RequestApartmentDetailDto requestDto) throws UnsupportedEncodingException {
+		String dealYearAndMonth = Integer.toString(requestDto.getDealYear()) + Integer.toString(requestDto.getDealMonth());
+		StringBuilder urlBuilder = new StringBuilder();
+		return urlBuilder.append(domainName).append(path)
+			.append("?").append(encode("serviceKey", encoder)).append(accessKey)
+			.append("&").append(encode("pageNo", encoder)).append("=").append(encode(pageNo, encoder))
+			.append("&").append(encode("numOfRows", encoder)).append("=").append(encode(numOfRows, encoder))
+			.append("&").append(encode("LAWD_CD", encoder)).append("=").append(encode(requestDto.getRegionalCode(), encoder))
+			.append("&").append(encode("DEAL_YMD", encoder)).append("=").append(encode(dealYearAndMonth, encoder))
+			.toString();
+	}
+
+	private String request(URL url) throws IOException {
+		HttpURLConnection httpURLConnection = getHttpURLConnection(url);
+		String response = getHttpResponse(httpURLConnection);
+		httpURLConnection.disconnect();
+		return response;
+	}
+
+	private HttpURLConnection getHttpURLConnection(URL url) throws IOException {
+		HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+		httpURLConnection.setRequestProperty("Content-type", "application/json");
+		return httpURLConnection;
+	}
+
+	private String getHttpResponse(HttpURLConnection httpURLConnection) throws IOException {
+		BufferedReader rd = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		StringBuilder response = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+			response.append(line);
+		}
+		rd.close();
+
+		return response.toString();
+	}
+}
---
+++ src/test/java/kancho/realestate/utils/api/storeaprtment/ApartmentApiClientTest.java
@@ -0,0 +1,67 @@
+package kancho.realestate.utils.api.storeaprtment;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kancho.realestate.comparingprices.domain.dto.request.RequestApartmentDetailDto;
+import kancho.realestate.comparingprices.domain.model.ApartmentDetail;
+import kancho.realestate.comparingprices.exception.InvalidDealYearAndMonthException;
+import kancho.realestate.comparingprices.exception.InvalidRegionalCodeException;
+
+@SpringBootTest(classes = ApartmentApiClient.class)
+class ApartmentApiClientTest {
+
+	@Autowired
+	private ApartmentApiClient apartmentApiClient;
+	private final String existingRegionalCode = "11110";
+	private final String notExistingRegionalCode = "4444444";
+	private final int correctDealYear = 2015;
+	private final int correctDealMonth = 12;
+	private final int notExistingDealMonth = 44;
+	private final int futureDealYear = 9999;
+
+	@Test
+	void 존재하는_지역코드와_올바른_계약날짜를_입력하면_아파트정보_요청_성공() throws JAXBException, IOException {
+		RequestApartmentDetailDto requestDto = new RequestApartmentDetailDto(existingRegionalCode, correctDealYear,
+			correctDealMonth);
+
+		List<ApartmentDetail> apartmentDetailList = apartmentApiClient.getApartmentDetails(requestDto);
+
+		assertThat(apartmentDetailList.get(0).toString())
+			.contains(existingRegionalCode);
+	}
+
+	@Test
+	void 존재하지_않는_지역코드를_입력하면_예외_발생() {
+		assertThatThrownBy(() -> {
+			RequestApartmentDetailDto requestDto = new RequestApartmentDetailDto(notExistingRegionalCode,
+				correctDealYear, correctDealMonth);
+		}).isInstanceOf(InvalidRegionalCodeException.class);
+	}
+
+	@Test
+	void 달력에_존재하지_않는_계약월을_입력하면_예외_발생() {
+		assertThatThrownBy(() -> {
+			RequestApartmentDetailDto requestDto = new RequestApartmentDetailDto(existingRegionalCode, correctDealYear,
+				notExistingDealMonth);
+		}).isInstanceOf(InvalidDealYearAndMonthException.class);
+	}
+
+	@Test
+	void 현재_보다_미래의_계약날짜를_입력하면_예외_발생() {
+		// 현재 연도와 동일한 연도, 미래의 월을 입력해도 동일한 오류가 발생한다.
+		assertThatThrownBy(() -> {
+			RequestApartmentDetailDto requestDto = new RequestApartmentDetailDto(existingRegionalCode, futureDealYear,
+				correctDealMonth);
+		}).isInstanceOf(InvalidDealYearAndMonthException.class);
+	}
+
+}
